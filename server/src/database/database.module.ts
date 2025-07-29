c046f9f24c1b76a449824c0cfa8f611e5c5056be
Matheus Henrique Braga Julidori <matheus.julidori@inatel.br>
2025-07-29 18:32:17 -0300

    feat: add database module with TypeORM and PostgreSQL configuration
    - Added DatabaseModule to handle database connections using TypeORM.
    - Integrated @nestjs/config for environment variable management.
    - Updated app.module.ts to include DatabaseModule and ClientModule.
    - Added necessary dependencies for TypeORM and PostgreSQL in package.json.
    - Enabled strict mode in TypeScript configuration for better type safety.

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
