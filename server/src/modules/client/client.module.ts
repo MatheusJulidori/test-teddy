27cf169fabe48463c52d7a93e583f197dc45c4f0
Matheus Henrique Braga Julidori <matheus.julidori@inatel.br>
2025-07-29 19:34:19 -0300

    fix: add Prometheus counter for total client creations in ClientModule

--- server/src/modules/client/client.module.ts
+++ server/src/modules/client/client.module.ts
@@ -3,10 +3,17 @@ import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Client } from './entities/client.entity';
+import { makeCounterProvider } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Client])],
   controllers: [ClientController],
-  providers: [ClientService],
+  providers: [
+    ClientService,
+    makeCounterProvider({
+      name: 'client_create_total',
+      help: 'Total de clientes criados',
+    }),
+  ],
 })
 export class ClientModule {}
