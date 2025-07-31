9cb13e346e003bd853fe72d1f7ea7cef11612e15
Matheus Henrique Braga Julidori <matheus.julidori@inatel.br>
2025-07-31 10:45:12 -0300

    feat: update Swagger setup to include versioning in the API path

--- server/src/main.ts
+++ server/src/main.ts
@@ -32,7 +32,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(process.env.PORT ?? 3000);
 }
